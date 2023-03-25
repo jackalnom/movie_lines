cb01da5c13ab15c02f67c84ca181dadb224c2688
jackalnom <jackalnom@gmail.com>
2023-03-25 14:08:44 -0700

    Added test case for movies.

--- test/test_movies.py
+++ test/test_movies.py
@@ -7,17 +7,17 @@ import json
 client = TestClient(app)
 
 
-def test_get_character():
-    response = client.get("/characters/7421")
+def test_get_movie():
+    response = client.get("/movies/44")
     assert response.status_code == 200
 
-    with open("test/characters/7421.json", encoding="utf-8") as f:
+    with open("test/movies/44.json", encoding="utf-8") as f:
         assert response.json() == json.load(f)
 
 
-def test_characters():
-    response = client.get("/characters/")
+def test_movies():
+    response = client.get("/movies/")
     assert response.status_code == 200
 
-    with open("test/characters/root.json", encoding="utf-8") as f:
+    with open("test/movies/root.json", encoding="utf-8") as f:
         assert response.json() == json.load(f)
