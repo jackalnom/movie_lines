ae52824d2b6af971ed46ce2521aa9d17778f2f8d
jackalnom <jackalnom@gmail.com>
2023-06-21 10:40:23 -0700

    Revert "Added initial API definitions"
    This reverts commit a7c5067308fcc4aba892a962fa0d1749923338da.

--- src/api/server.py
+++ src/api/server.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
-from src.api import carts, pkg_util, catalog, b2b
+from src.api import characters, movies, pkg_util, conversations
+from starlette.middleware.cors import CORSMiddleware
 
 description = """
-Central Coast Cauldrons is the premier ecommerce site for all your alchemical desires.
+Movie API returns dialog statistics on top hollywood movies from decades past.
+
+## Characters
+
+You can:
+* **list characters with sorting and filtering options.**
+* **retrieve a specific character by id**
+
+## Movies
+
+You can:
+* **list movies with sorting and filtering options.**
+* **retrieve a specific movie by id**
+* **you can add additional conversations to a movie by id**
 """
 tags_metadata = [
     {
@@ -15,12 +29,10 @@ tags_metadata = [
     },
 ]
 
-# Implement API Key Authentication
-
 app = FastAPI(
-    title="Central Coast Cauldrons",
+    title="Movie Lines API",
     description=description,
-    version="0.0.1",
+    version="0.0.2",
     terms_of_service="http://example.com/terms/",
     contact={
         "name": "Lucas Pierce",
@@ -29,12 +41,23 @@ app = FastAPI(
     openapi_tags=tags_metadata,
 )
 
-app.include_router(carts.router)
-app.include_router(catalog.router)
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(characters.router)
+app.include_router(conversations.router)
+app.include_router(movies.router)
 app.include_router(pkg_util.router)
-app.include_router(b2b.router)
 
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the Central Coast Cauldrons."}
+    return {"message": "Welcome to the Movie API. See /docs for more information."}
